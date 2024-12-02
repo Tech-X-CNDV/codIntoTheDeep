bdfeec68d25263135b39920be9c1a39729ecfa86
TonyJoaca <dj_tony2013@yahoo.ro>
2024-12-02 17:27:33 +0200

    v3.5
    Added intake/outtakeAxons, intakeClaw and intakeSliders classes to control each of them respectively
    
    Reverse one encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomieRED_COS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomieRED_COS.java
@@ -45,7 +45,7 @@ public class autonomieRED_COS extends LinearOpMode{
                 // checks lift's current position
                 double pos = outtakeSliderUp.getCurrentPosition();
                 packet.put("outtakePos", pos);
-                if (pos < 4500.0) {
+                if (pos < 4150.0) {
                     // true causes the action to rerun
                     return true;
                 } else {
@@ -73,7 +73,7 @@ public class autonomieRED_COS extends LinearOpMode{
 
                 double pos = outtakeSliderUp.getCurrentPosition();
                 packet.put("outtakePos", pos);
-                if (pos > 1.0) {
+                if (pos > 0.0) {
                     return true;
                 } else {
                     outtakeSliderUp.setPower(0);
@@ -84,6 +84,42 @@ public class autonomieRED_COS extends LinearOpMode{
         }
         public Action slidersDown(){
             return new SlidersDown();
+        }
+    }
+
+    public class OuttakeAxon {
+        private Servo outtakeAxonLeft;
+        private Servo outtakeAxonRight;
+
+        public OuttakeAxon(HardwareMap hardwareMap){
+            outtakeAxonLeft = hardwareMap.get(Servo.class, "outtakeAxonLeft");
+            outtakeAxonRight = hardwareMap.get(Servo.class, "outtakeAxonRight");
+        }
+
+        public class AxonUp implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                outtakeAxonLeft.setPosition(1);
+                outtakeAxonRight.setPosition(1);
+                return false;
+            }
+        }
+
+        public Action axonUp(){
+            return new AxonUp();
+        }
+
+        public class AxonDown implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                outtakeAxonLeft.setPosition(0.4);
+                outtakeAxonRight.setPosition(0.4);
+                return false;
+            }
+        }
+
+        public Action axonDown(){
+            return new AxonDown();
         }
     }
 
@@ -117,12 +153,142 @@ public class autonomieRED_COS extends LinearOpMode{
         }
     }
 
+    public class IntakeSliders {
+        private DcMotor intakeSlider;
+
+        public IntakeSliders(HardwareMap hardwareMap) {
+            intakeSlider = hardwareMap.get(DcMotor.class, "intakeSlider");
+            intakeSlider.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            intakeSlider.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        public class SlidersUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    intakeSlider.setPower(1);
+                    initialized = true;
+                }
+                // checks lift's current position
+                double pos = intakeSlider.getCurrentPosition();
+                packet.put("intakePos", pos);
+                if (pos < 680.0) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    intakeSlider.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action slidersUp() {
+            return new SlidersUp();
+        }
+
+        public class SlidersDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    intakeSlider.setPower(-1);
+                    initialized = true;
+                }
+
+                double pos = intakeSlider.getCurrentPosition();
+                packet.put("intakePos", pos);
+                if (pos > 0.0) {
+                    return true;
+                } else {
+                    intakeSlider.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action slidersDown(){
+            return new SlidersDown();
+        }
+    }
+
+    public class IntakeAxon {
+        private Servo intakeAxonLeft;
+        private Servo intakeAxonRight;
+
+        public IntakeAxon(HardwareMap hardwareMap){
+            intakeAxonLeft = hardwareMap.get(Servo.class, "intakeAxonLeft");
+            intakeAxonRight = hardwareMap.get(Servo.class, "intakeAxonRight");
+        }
+
+        public class AxonUp implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intakeAxonLeft.setPosition(0);
+                intakeAxonRight.setPosition(0);
+                return false;
+            }
+        }
+
+        public Action axonUp(){
+            return new AxonUp();
+        }
+
+        public class AxonDown implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intakeAxonLeft.setPosition(0.27);
+                intakeAxonRight.setPosition(0.27);
+                return false;
+            }
+        }
+
+        public Action axonDown(){
+            return new AxonDown();
+        }
+    }
+
+    public class IntakeClaw {
+        private Servo claw;
+
+        public IntakeClaw(HardwareMap hardwareMap) {
+            claw = hardwareMap.get(Servo.class, "ghiaraIntake");
+        }
+
+        public class CloseClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                claw.setPosition(0.3);
+                return false;
+            }
+        }
+        public Action closeClaw() {
+            return new CloseClaw();
+        }
+
+        public class OpenClaw implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                claw.setPosition(0);
+                return false;
+            }
+        }
+        public Action openClaw() {
+            return new OpenClaw();
+        }
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap,new Pose2d(0,0,0));
         Pose2d initialPose = new Pose2d(-36, -56, Math.toRadians(90));
         OuttakeSliders outtakeSliders = new OuttakeSliders(hardwareMap);
+        IntakeSliders intakeSliders = new IntakeSliders(hardwareMap);
         OuttakeClaw outtakeClaw = new OuttakeClaw(hardwareMap);
+        IntakeClaw intakeClaw = new IntakeClaw(hardwareMap);
+        OuttakeAxon outtakeAxon = new OuttakeAxon(hardwareMap);
+        IntakeAxon intakeAxon = new IntakeAxon(hardwareMap);
 
         TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
                 .turn(Math.toRadians(-45))
@@ -138,8 +304,16 @@ public class autonomieRED_COS extends LinearOpMode{
 
         int visionOutputPosition = 1; // dummy value (temporary)
 
-        Actions.runBlocking(outtakeSliders.slidersDown());
-        Actions.runBlocking(outtakeClaw.closeClaw());
+        Actions.runBlocking(
+                new SequentialAction(
+                        outtakeSliders.slidersDown(),
+                        intakeSliders.slidersDown(),
+                        outtakeClaw.closeClaw(),
+                        intakeClaw.closeClaw(),
+                        outtakeAxon.axonDown(),
+                        intakeAxon.axonUp()
+                )
+        );
 
         while (!isStopRequested() && !opModeIsActive()) { // these lines until waitforstart() can be ignored.
             int position = visionOutputPosition;

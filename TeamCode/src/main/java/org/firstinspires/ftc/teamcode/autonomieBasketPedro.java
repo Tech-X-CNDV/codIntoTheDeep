a12e15f4df25e3e5d18a20163092256fded67dcd
alinutul <ulicialin123@gmail.com>
2025-01-15 13:19:56 +0200

    v8.0
    More work on autnomous bakset code to make it work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomieBasketPedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomieBasketPedro.java
@@ -40,19 +40,19 @@ public class autonomieBasketPedro extends OpMode{
     private final Pose scorePoseControl = new Pose(50,89);
 
     private final Pose moveNextToFirst = new Pose(61,122);
-    private final Pose moveNextToFirstControl = new Pose(39,83);
+    private final Pose moveNextToFirstControl = new Pose(38.5,87.7);
 
-    private final Pose pushFirst = new Pose(12,122);
+    private final Pose pushFirst = new Pose(11,122);
 
     private final Pose pushSecond1 = new Pose(61,122);
     private final Pose pushSecond2 = new Pose(61,131);
-    private final Pose pushSecond3 = new Pose(20,131);
+    private final Pose pushSecond3 = new Pose(16,131);
 
     private final Pose pushThird1 = new Pose(61,131);
     private final Pose pushThird2 = new Pose(61,137.5);
-    private final Pose pushThird3 = new Pose(30,137.5);
+    private final Pose pushThird3 = new Pose(16,137.5);
 
-    private final Pose parkPose = new Pose(60,94);
+    private final Pose parkPose = new Pose(60,97);
     private final Pose parkControl = new Pose(63,126);
 
     //
@@ -60,15 +60,18 @@ public class autonomieBasketPedro extends OpMode{
     public void buildPaths() {
         Score = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(startPose),new Point(scorePoseControl),new Point(scorePosePreLoad)))
-                .setLinearHeadingInterpolation(Math.toRadians(-180),Math.toRadians(-45))
+                .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(-45))
+                .setPathEndTimeoutConstraint(1.0)
                 .build();
         goToFirst = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(scorePosePreLoad),new Point(moveNextToFirstControl),new Point(moveNextToFirst)))
                 .setLinearHeadingInterpolation(Math.toRadians(-45),Math.toRadians(0))
+                .setPathEndTimeoutConstraint(1.0)
                 .build();
         goToBasketFirst = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(moveNextToFirst),new Point(pushFirst)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
+                .setPathEndTimeoutConstraint(1.0)
                 .build();
         goPushSecond = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(pushFirst),new Point(pushSecond1)))
@@ -77,6 +80,7 @@ public class autonomieBasketPedro extends OpMode{
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(new BezierLine(new Point(pushSecond2),new Point(pushSecond3)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
+                .setPathEndTimeoutConstraint(5.0)
                 .build();
         goPushThird = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(pushSecond3),new Point(pushThird1)))
@@ -85,44 +89,72 @@ public class autonomieBasketPedro extends OpMode{
                 .setConstantHeadingInterpolation(Math.toRadians(0))
                 .addPath(new BezierLine(new Point(pushThird2),new Point(pushThird3)))
                 .setConstantHeadingInterpolation(Math.toRadians(0))
+                .setPathEndTimeoutConstraint(5.0)
                 .build();
         goPark = follower.pathBuilder()
                 .addPath(new BezierCurve(new Point(pushThird3),new Point(parkControl),new Point(parkPose)))
                 .setLinearHeadingInterpolation(Math.toRadians(0),Math.toRadians(270))
+                .setPathEndTimeoutConstraint(1.0)
                 .build();
     }
     public void autonomousPathUpdate(){
         switch (pathState) {
             case 0:
                 //aici adaug miscarea sliderelor
+                axon.SetIntakePosition(RobotConstants.intakeDownPos);
                 claw.CloseOuttake();
+                slider.MoveOuttake(RobotConstants.outtakeSliderExtendPosition, 1);
+                axon.SetOuttakePosition(RobotConstants.outtakeUpPos);
                 follower.followPath(Score, true);
                 setPathState(1);
                 break;
             case 1:
-                if(follower.getPose().getX() > (scorePosePreLoad.getX() - 1) && follower.getPose().getY() > (scorePoseControl.getY() - 1))
+                if(follower.getPose().getX() > (scorePosePreLoad.getX() - 2) && follower.getPose().getY() > (scorePosePreLoad.getY() - 2))
+                {
                     claw.OpenOuttake();
+                //if(pathTimer.getElapsedTimeSeconds()>3.0) {
+                //    slider.MoveOuttake(RobotConstants.outtakeSliderRetractPosition, 1);
+                //    axon.SetOuttakePosition(RobotConstants.outtakeMidPos);
+                //}
                 follower.followPath(goToFirst, true);
                 setPathState(2);
+               } break;
             case 2:
-                if(follower.getPose().getX() > (moveNextToFirst.getX() - 1) && follower.getPose().getY() < (moveNextToFirst.getY() + 1))
+                if(follower.getPose().getX() > (moveNextToFirst.getX() - 2) && follower.getPose().getY() < (moveNextToFirst.getY() + 2))
+                {   slider.MoveOuttake(RobotConstants.outtakeSliderRetractPosition, 1);
+                    axon.SetOuttakePosition(RobotConstants.outtakeMidPos);
                     follower.followPath(goToBasketFirst,true);
                 setPathState(3);
-            case 3:
-                if(follower.getPose().getX() < (pushFirst.getX() + 1) && follower.getPose().getY() > (pushFirst.getY() - 1))
+                }break;
+
+                case 3:
+                if(follower.getPose().getX() < (pushFirst.getX() + 1) && follower.getPose().getY() < (pushFirst.getY() + 1))
+                {
+                   // if(pathTimer.getElapsedTimeSeconds()>4.9)
                     follower.followPath(goPushSecond,true);
-                setPathState(4);
+                setPathState(4);}
+                break;
             case 4:
-                if(follower.getPose().getX() > (pushSecond3.getX() - 1) && follower.getPose().getY() > (pushSecond3.getY() - 1))
+                if(follower.getPose().getX() < (pushSecond3.getX() + 1) && follower.getPose().getY() > (pushSecond3.getY() - 1))
+                {
+                    //if(pathTimer.getElapsedTimeSeconds()>4.9)
                     follower.followPath(goPushThird,true);
-                setPathState(5);
+                setPathState(5);}
+                break;
             case 5:
-                if(follower.getPose().getX() > (pushThird3.getX() - 1) && follower.getPose().getY() > (pushThird3.getY() - 1))
+                if(follower.getPose().getX() < (pushThird3.getX() + 1) && follower.getPose().getY() > (pushThird3.getY() - 1))
+                {
+                    axon.SetIntakePosition(RobotConstants.intakeUpPos);
                     follower.followPath(goPark,true);
-                setPathState(6);
+                    setPathState(6);
+                }
+                break;
             case 6:
-                if(follower.getPose().getX() > (parkPose.getX() - 1) && follower.getPose().getY() < (parkPose.getY() + 1))
+                if(follower.getPose().getX() > (parkPose.getX() - 1) && follower.getPose().getY() < (parkPose.getY() + 1)) {
+                    slider.MoveIntake(RobotConstants.intakeSliderRetractPosition,1);
                     setPathState(-1);
+                }
+                break;
         }
     }
 
@@ -171,7 +203,7 @@ public class autonomieBasketPedro extends OpMode{
         slider.InitOuttake();
         axon = new AxonSubsystem(hardwareMap);
         axon.InitIntake();
-        axon.InitOuttake(RobotConstants.outtakeUpPos);
+        axon.InitOuttake(RobotConstants.outtakeMidPos);
 
         buildPaths();
     }
